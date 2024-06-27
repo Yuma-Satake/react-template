fffa357c1b8f465f9aa8f4d81f4594ef113a5367
Yuma Satake <satous.y@icloud.com>
2024-06-27 21:40:33 +0900

    fix: lintを修正

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,8 @@
 export default {
-  '**/*.{html,css,js,jsx,ts,tsx,json,yml,yaml,md}': [() => 'pnpm check'],
+  '**/*.{js,jsx,ts,tsx}': [
+    'eslint --fix --ext .js,.jsx,.ts,.tsx --report-unused-disable-directives --max-warnings 0',
+    'prettier --write',
+    'git add',
+  ],
+  '**/*.{html,css,json,yml,yaml,md}': ['prettier --write', 'git add'],
 };
