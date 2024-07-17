4cdbdef093320cd29dc3ac67a8fdbc8106c31c46
Yuma Satake <satous.y@icloud.com>
2024-07-17 09:40:34 +0900

    feat: package.jsonに書き込むように変更

--- init-setup.js
+++ init-setup.js
@@ -1,5 +1,4 @@
 /* eslint-disable no-console */
-
 import fs from 'fs';
 import inquirer from 'inquirer';
 
@@ -7,7 +6,7 @@ const questions = [
   {
     type: 'list',
     name: 'commitLint',
-    message: 'commit lint が必要ですか？(husky, lint-staged)',
+    message: 'commit lint が必要ですか？',
     choices: ['yes', 'no'],
   },
   {
@@ -16,11 +15,26 @@ const questions = [
     message: 'dependabot が必要ですか？',
     choices: ['yes', 'no'],
   },
+  {
+    type: 'input',
+    name: 'repositoryURL',
+    message: 'リポジトリのURLをペーストして下さい',
+  },
 ];
 
 inquirer.prompt(questions).then((answers) => {
+  const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf-8'));
+
+  const { repositoryURL } = answers;
+  const [, , , repositoryOrganization, repositoryName] = repositoryURL.split('/');
+  packageJson.name = repositoryName;
+  packageJson.repository.url = `git+https://github.com/${repositoryOrganization}/${repositoryName}.git`;
+
+  fs.writeFileSync('package.json', JSON.stringify(packageJson, null, 2));
+  console.log('✅package.jsonを更新しました。');
+
   if (answers.commitLint === 'no') {
-    fs.rmdirSync('./husky', { recursive: true });
+    fs.rmSync('./.husky', { recursive: true, force: true });
     console.log('✅commitLintを削除しました。');
     fs.unlinkSync('lint-staged.config.js');
     console.log('✅lint-staged.config.jsを削除しました。');
